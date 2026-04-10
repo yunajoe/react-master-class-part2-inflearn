c5873862b3e935545cc927cdbb36fdf8baca39e7
yunajoe <yunajoe@gmail.com>
2026-04-10 21:02:48 +0900

    guide:Context 분리와 리렌더링 최적화

---
+++ src/provider/AuthPerformanceProvider.jsx
@@ -0,0 +1,36 @@
+import { useMemo, useReducer } from "react";
+import {
+  AuthDispatchContext,
+  AuthStateContext,
+} from "../contexts/AuthPerformanceContext";
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN_START":
+      return { user: null, isLoading: true, error: null };
+    case "LOGIN_SUCCESS":
+      return { user: action.payload, isLoading: false, error: null };
+    case "LOGIN_FAILURE":
+      return { user: null, isLoading: false, error: action.payload };
+    case "LOGOUT":
+      return { user: null, isLoading: false, error: null };
+    default:
+      return state;
+  }
+}
+
+function AuthPerformanceProvider({ children }) {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    isLoading: false,
+    error: null,
+  });
+  const memoizedState = useMemo(() => state, [state]);
+  return (
+    <AuthStateContext.Provider value={memoizedState}>
+      <AuthDispatchContext value={dispatch}>{children}</AuthDispatchContext>
+    </AuthStateContext.Provider>
+  );
+}
+
+export default AuthPerformanceProvider;
