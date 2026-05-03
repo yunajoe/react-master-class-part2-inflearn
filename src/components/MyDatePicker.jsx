0b85a4cdd94af4bdbc86cc29c7513a5a4944480a
yunajoe <yunajoe@gmail.com>
2026-05-03 14:37:17 +0900

    mission:외부 라이브러리 연동 — 명령적 API 포장 전략

--- src/components/MyDatePicker.jsx
+++ src/components/MyDatePicker.jsx
@@ -2,15 +2,23 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css"; // 날짜
 import { useEffect, useImperativeHandle, useRef } from "react";
 
-function MyDatePicker({ ref }) {
+function MyDatePicker({ ref, onChange }) {
   const inputRef = useRef();
   const libraryRef = useRef();
 
   // ref 를 라이브러리와 연결 시킨다.
   useEffect(() => {
-    libraryRef.current = flatpickr(inputRef.current, { onChange: () => {} });
+    libraryRef.current = flatpickr(inputRef.current, {
+      onChange: (dates, currentDateString, self, data) => {
+        onChange(currentDateString);
+      },
+    });
 
-    return () => {};
+    return () => {
+      if (libraryRef.current) {
+        libraryRef.current.destroy();
+      }
+    };
   }, []);
 
   useImperativeHandle(
@@ -19,15 +27,17 @@ function MyDatePicker({ ref }) {
       // 부모가 조종할 handler
       open: () => {
         if (libraryRef.current) {
-          console.log("TEST");
+          libraryRef.current.open();
         }
       },
       close: () => {
         if (libraryRef.current) {
+          libraryRef.current.close();
         }
       },
       init: () => {
         if (libraryRef.current) {
+          libraryRef.current.clear();
         }
       },
     }),
@@ -35,7 +45,18 @@ function MyDatePicker({ ref }) {
   );
   return (
     <div>
-      <input ref={inputRef} />
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="날짜를 선택하세요"
+        style={{
+          padding: "10px",
+          borderRadius: "8px",
+          border: "2px solid #ddd",
+          width: "250px",
+          fontSize: "16px",
+        }}
+      />
     </div>
   );
 }
--- src/components/MyDatePickerWrapper.jsx
+++ src/components/MyDatePickerWrapper.jsx
@@ -1,16 +1,22 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import MyDatePicker from "./MyDatePicker";
 
 function MyDatePickerWrapper() {
   const controlRef = useRef();
+  const [selectedDate, setSelectedDate] = useState("");
+
+  const handleChangeDate = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
     <div>
       <h1>데이트 피커 원격 제어</h1>
-      <h3>선택된 날짜</h3>
+      <h3>선택된 날짜 {selectedDate || "없음"}</h3>
       <button onClick={() => controlRef.current.open()}>창 열기</button>
       <button onClick={() => controlRef.current.close()}>창 닫기</button>
       <button onClick={() => controlRef.current.init()}>날짜 초기화</button>
-      <MyDatePicker ref={controlRef} />
+      <MyDatePicker ref={controlRef} onChange={handleChangeDate} />
     </div>
   );
 }
